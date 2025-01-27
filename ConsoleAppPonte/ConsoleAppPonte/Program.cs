de4c9bc0d84bb5a1132d0d0a1bb584afd12134d7
maria.rossi <maria.rossi@PC1020BIS>
2025-01-27 09:02:47 +0100

    aggiunta progetto ponte

---
+++ ConsoleAppPonte/ConsoleAppPonte/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleAppPonte
+{
+    internal class Program
+    {
+        //ponte che permette passaggio di veicoli, più di uno alla volta per corsia
+        //in ciascuna corsia invece può passare una macchina alla volta
+        static void Main(string[] args)
+        {
+        }
+    }
+}
